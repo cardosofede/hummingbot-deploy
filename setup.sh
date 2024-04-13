b65ad0da15e9294897460e0a926d2b5a216bf58f
cardosofede <federico.cardoso.e@gmail.com>
2024-04-13 16:01:36 -0300

    (feat) add setup.sh file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pulling the required Docker images
+docker pull dardonacci/hummingbot
+docker pull dardonacci/backend-api
+docker pull dardonacci/dashboard
+
+# Creating .env file with the required environment variables
+echo "CONFIG_PASSWORD=a" > .env
+echo "BOTS_PATH=$(pwd)" >> .env
+
+# Running docker-compose in detached mode
+docker-compose up -d
